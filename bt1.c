e8b7caec05e4c26c1e53d42dc361a6befa9ea8d6
devap247@gmail.com <devapp247@gmail.com>
2022-11-17 14:49:21 +0700

    Create bt1.c
    Them bai tap 1

---
+++ bt1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+//HDH trao quyền đk cho hàm main
+int main()
+{	
+	int a =0;
+	printf("Hello world!");
+	//scanf("%d",&a);
+	printf("\nGia tri cua a %d",a);
+	findProductById(1);//gọi hàm tư main
+	return 0;
+}
+void findProductById(int id){
+	//coding here 
+}
